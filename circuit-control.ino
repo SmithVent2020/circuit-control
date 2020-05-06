98ea2d90799d97c02ec70a6e190763783c91622e
Farida Sabry <farida.sabry2@gmail.com>
2020-05-06 18:15:41 -0400

    Initial file

---
+++ circuit-control.ino
@@ -0,0 +1,17 @@
+/**
+ * SmithVent
+ * Initial Ventilator Control Circuit
+ * 
+ * modified on May 6, 2020
+ * created by Dan, Farida, and Naylani
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
