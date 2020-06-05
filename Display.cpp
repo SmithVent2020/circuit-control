8e374bed4c6dcc97200324625ff60e3f42761896
Farida Sabry <farida.sabry2@gmail.com>
2020-06-05 19:44:37 -0400

    another typo

--- Display.cpp
+++ Display.cpp
@@ -16,7 +16,7 @@ Display::Display() {
   settings.volume = TIDAL_VOLUME;
   settings.inspHold = false;
   settings.peak = PS_MIN;
-  settings.apnea = APNEA_BACKUP/1000;;
+  settings.apnea = APNEA_BACKUP/1000;
   settings.cycleOff = CYCLE_OFF;
   settings.riseTime = RISE_TIME/1000;
 
