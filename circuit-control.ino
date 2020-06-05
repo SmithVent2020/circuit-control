723a1060fda17e12312aaef8fbbe861ad7b5d677
Farida Sabry <farida.sabry2@gmail.com>
2020-06-04 21:19:50 -0400

    missing parens

--- circuit-control.ino
+++ circuit-control.ino
@@ -264,7 +264,7 @@ void beginInspiration() {
   display.writeVolumeInsp(inspFlowReader.getVolume());
   display.writeVolumeExp(expFlowReader.getVolume()); 
   display.writeBPM(CC_PER_MS_TO_LPM/cycleDuration);
-  display.writeMinuteVolume(inspFlowReader.getVolume()/cycleDuration) * CC_PER_MS_TO_LPM;
+  display.writeMinuteVolume((inspFlowReader.getVolume()/cycleDuration) * CC_PER_MS_TO_LPM);
 
   // close expiratory valve
   expValve.close();
