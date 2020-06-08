bf10b3d0df891c258c0e24b65e9b30858a6f24e8
Naylani <naylanihw@gmail.com>
2020-06-07 20:50:09 -0400

    put back pressure trigger

--- circuit-control.ino
+++ circuit-control.ino
@@ -587,7 +587,7 @@ void volumeControlStateMachine(){
       //Serial.print("patientTriggered?"); Serial.print("\t"); Serial.println(patientTriggered); //@debugging
       //Serial.print("targetCycleEndTime"); Serial.print("\t"); Serial.println(targetCycleEndTime); //@debugging
 
-      if (timeout) { //@debugging add back with real patient: patientTriggered ||
+      if (patientTriggered || timeout) { //@debugging add back with real patient: 
         if (!patientTriggered){
 
         }
